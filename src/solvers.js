c3d94c152bb6d436319c6a61e3667a4eb7b33b95
jdstep <jdstep23@gmail.com>
2015-05-18 19:25:56 -0700

    Finds a single valid solution for rooks

--- src/solvers.js
+++ src/solvers.js
@@ -14,44 +14,56 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  // var solution = undefined; //fixme
-
-  // iterative solution
-  // iterate over board length
-    // generate random column
-    // if there is no conflict
-      // set piece there
   var solution = new Board({n: n});
-  var currentRow;
   var columnToPlace;
-
-  debugger;
-
-  // finds an empty column given a board and rowIndex
-  // check if has any conflicts && horizontal && and vertical
-  // ASSUME THERE IS A PLACE WE CAN PUT IT
-  var findEmptyColumn = function(board, row) {
-    // declare emptyColumnLoc
-    var emptyColumnLoc;
-    // iterate over each column
-    for (var i = 0; i < row.length; i++) {
-
-    }
-    // if we can place a piece in that place by checking for conflicts && horizontal && vertical
-      // return emptyColumnLoc
-  };
-
-  // TODO: rewrite this to avoid using recusion
-  for (var i = 0; i < n; i++) {
-    currentRow =  solution.get(i);
-    columnToPlace = findEmptyColumn(solution, currentRow);
-    solution.togglePiece(i, columnToPlace);
-  }
 
   // debugger;
 
+  // must pass in a row and column that is not currently occupied
+  // tests if current passed in row and column has any rook conflicts
+  var rookConflicts = function(rowIndex, columnIndex) {
+    solution.togglePiece(rowIndex, columnIndex);
+
+    if (solution.hasAnyRowConflicts() ||
+        solution.hasAnyColConflicts()) {
+      solution.togglePiece(rowIndex, columnIndex);
+      return true;
+    } else {
+      solution.togglePiece(rowIndex, columnIndex);
+      return false;
+    }
+  };
+
+  // finds an empty column given a board and rowIndex
+  // pass in entire row
+  // check if has any conflicts && horizontal && and vertical
+  // ASSUME THE ROW IS EMPTY
+  var findEmptyColumn = function(rowIndex) {
+    // iterate over each column
+    for (var j = 0; j < n; j++) {
+      if (rookConflicts(rowIndex, j) === false) {
+        // returns column index that does not cause conflicts
+        return j;
+      }
+    }
+  };
+
+
+
+  // iterates over each row to find a conflict-free place to put a rook
+  var placeRooks = function() {
+    for (var i = 0; i < n; i++) {
+      columnToPlace = findEmptyColumn(i);
+      solution.togglePiece(i, columnToPlace);
+    }
+  };
+
+  placeRooks();
+
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+
+  // returns an array of arrays of the solution board object
+  return solution.rows();
 };
 
 
@@ -59,6 +71,9 @@ window.findNRooksSolution = function(n) {
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
+
+
+
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
